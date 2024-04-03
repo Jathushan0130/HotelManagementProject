432a286f255fb266596b5cc06d5ecb108d022853
AydinVuu <90784581+AydinVuu@users.noreply.github.com>
2024-04-02 21:06:33 -0400

    Update ProvidedServiceService.java

---
+++ src/main/java/service/ProvidedServiceService.java
@@ -0,0 +1,41 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import model.ProvidedService;
+import repository.ServiceRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProvidedServiceService {
+
+    private final ServiceRepository serviceRepository;
+
+    @Autowired
+    public ProvidedServiceService(ServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+    public List<ProvidedService> getAllProvidedServices() {
+        return serviceRepository.findAll();
+    }
+
+    public void addProvidedService(ProvidedService service) {
+        serviceRepository.save(service);
+    }
+
+    public Optional<ProvidedService> getProvidedServiceById(Long id) {
+        return serviceRepository.findById(id);
+    }
+
+    public void updateProvidedService(ProvidedService service) {
+        serviceRepository.save(service);
+    }
+
+    public void deleteProvidedServiceById(Long id) {
+        serviceRepository.deleteById(id);
+    }
+
+}
