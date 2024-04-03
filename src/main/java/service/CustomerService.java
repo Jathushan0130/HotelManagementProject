e9e69660b79925490807488fc70650a30308befc
AydinVuu <90784581+AydinVuu@users.noreply.github.com>
2024-04-02 21:05:12 -0400

    Update CustomerService.java (Jathu's code)

---
+++ src/main/java/service/CustomerService.java
@@ -0,0 +1,41 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import model.Customer;
+import repository.CustomerRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public void addCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public Optional<Customer> getCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public void updateCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public void deleteCustomerById(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+}
